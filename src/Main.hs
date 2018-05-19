6d52c27734d442c90cb50a87170840206aecf6a5
Silvan Mosberger <infinisil@icloud.com>
2018-05-19 23:52:52 +0200

    Changed back to non-working version

--- src/Main.hs
+++ src/Main.hs
@@ -1,38 +1,37 @@
 module Main where
 
-import System.Process.Text
-import qualified Data.Text as Text
-import Data.Text (Text)
-import System.Directory (listDirectory)
-import System.FilePath
-import Control.Concurrent.Async.Pool
-import System.Exit
-import Data.Maybe
-import qualified Data.Vector as V
-import Data.Bits
-import Data.Char
-import Data.List
-import Data.Ord
-import System.Environment
+import           Control.Concurrent.Async.Pool
+import           Data.Bits
+import           Data.Char
+import           Data.List
+import           Data.Maybe
+import           Data.Ord
+import           Data.Text                     (Text)
+import qualified Data.Text                     as Text
+import qualified Data.Vector                   as V
+import           System.Directory              (listDirectory)
+import           System.Environment
+import           System.Exit
+import           System.FilePath
+import           System.Process.Text
 
 blockhash = "/home/infinisil/src/nixpkgs/result/bin/blockhash"
 
-mapConcurrentlyBounded :: Traversable t => Int -> (a1 -> IO a) -> t a1 -> IO (t a)
-mapConcurrentlyBounded n f args = withTaskGroup n $ flip mapTasks (fmap f args)
-
 getHashes :: IO [(Text, String)]
 getHashes = do
+  pool <- createPool
+  group <- createTaskGroup pool 8
   dir <- head <$> getArgs
   files <- listDirectory dir
   let absolute = map (dir </>) files
-  catMaybes <$> mapConcurrentlyBounded 8 getHash absolute
+  catMaybes <$> mapConcurrently group getHash absolute
     where
       getHash :: FilePath -> IO (Maybe (Text, FilePath))
       getHash file = do
         (code, stdout, _) <- readProcessWithExitCode blockhash [ file ] Text.empty
         case code of
           ExitFailure _ -> return Nothing
-          ExitSuccess -> return $ Just (stdout, file)
+          ExitSuccess   -> return $ Just (stdout, file)
 
 mapResults :: (Text, FilePath) -> (Hash, FilePath)
 mapResults (text, fp) = (Hash (V.fromList (Text.unpack text)), fp)
@@ -66,4 +65,4 @@ search hash results = top
     newHash = Hash (V.fromList hash)
     y = map (\(h, f) -> (hammingDistance newHash h, f)) results
     top = filter (\(d, f) -> d <= 10) . sortBy (comparing fst) $ y
-  
+
