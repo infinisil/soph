21e198869c940d40cd095cf127d0d2f0bc659393
Silvan Mosberger <infinisil@icloud.com>
2018-06-13 04:38:42 +0200

    Some improvements
    - No more hardcoded binary path
    - Will take a file as input now
    - Error messages when wrong args/no blockhash binary found
    - It actually works now?! (Did I not test it before??)

--- src/Main.hs
+++ src/Main.hs
@@ -1,41 +1,56 @@
+{-# LANGUAGE FlexibleContexts  #-}
+{-# LANGUAGE NamedFieldPuns    #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+
 module Main where
 
-import System.Process.Text
-import qualified Data.Text as Text
-import Data.Text (Text)
-import System.Directory (listDirectory)
-import System.FilePath
-import Control.Concurrent.Async.Pool
-import System.Exit
-import Data.Maybe
-import qualified Data.Vector as V
-import Data.Bits
-import Data.Char
-import Data.List
-import Data.Ord
-import System.Environment
-
-blockhash = "/home/infinisil/src/nixpkgs/result/bin/blockhash"
+import           Control.Concurrent.Async.Pool
+import           Control.Monad
+import           Control.Monad.Reader
+import           Data.Bits
+import           Data.Char
+import           Data.List
+import           Data.Maybe
+import           Data.Ord
+import           Data.Text                     (Text)
+import qualified Data.Text                     as Text
+import qualified Data.Vector                   as V
+import           System.Directory
+import           System.Environment
+import           System.Exit
+import           System.FilePath
+import           System.Process.Text
 
 mapConcurrentlyBounded :: Traversable t => Int -> (a1 -> IO a) -> t a1 -> IO (t a)
 mapConcurrentlyBounded n f args = withTaskGroup n $ flip mapTasks (fmap f args)
 
-getHashes :: IO [(Text, String)]
-getHashes = do
-  dir <- head <$> getArgs
-  files <- listDirectory dir
-  let absolute = map (dir </>) files
-  catMaybes <$> mapConcurrentlyBounded 8 getHash absolute
-    where
-      getHash :: FilePath -> IO (Maybe (Text, FilePath))
-      getHash file = do
-        (code, stdout, _) <- readProcessWithExitCode blockhash [ file ] Text.empty
-        case code of
-          ExitFailure _ -> return Nothing
-          ExitSuccess -> return $ Just (stdout, file)
+data Config = Config
+  { files     :: [FilePath]
+  , blockhash :: FilePath
+  } deriving Show
 
-mapResults :: (Text, FilePath) -> (Hash, FilePath)
-mapResults (text, fp) = (Hash (V.fromList (Text.unpack text)), fp)
+getConfig :: IO Config
+getConfig = do
+  dir <- fromMaybe (fail "No argument supplied to use as a directory") . listToMaybe <$> getArgs
+  blockhash <- fromMaybe (fail "No hashblock binary found in PATH") <$> findExecutable "blockhash"
+  files <- fmap (dir </>) <$> listDirectory dir
+  return Config
+    { files = files
+    , blockhash = blockhash
+    }
+
+getHash :: Config -> FilePath -> IO (Maybe (Hash, FilePath))
+getHash Config { blockhash } file = do
+  (code, stdout, stderr) <- readProcessWithExitCode blockhash [ file ] Text.empty
+  case code of
+    ExitFailure code -> do
+      putStrLn $ "Error code " ++ show code ++ " while getting hash of file " ++ file ++ ": " ++ show stderr ++ show stdout
+      return Nothing
+    ExitSuccess   -> return $ Just (Hash (V.fromList (Text.unpack (fst (Text.breakOn "  " stdout)))), file)
+
+getHashes :: Config -> IO [(Hash, String)]
+getHashes config@Config { files } = catMaybes <$> mapConcurrentlyBounded 8 (getHash config) files
 
 type Results = [(Hash, FilePath)]
 
@@ -47,23 +62,33 @@ hammingDistance (Hash h1) (Hash h2) =
 
 main :: IO ()
 main = do
+  config <- getConfig
   putStrLn "Calculating Hashes.."
-  result <- fmap mapResults <$> getHashes
-  doSearch result
+  result <- getHashes config
+  doSearch config result
 
-doSearch :: Results -> IO ()
-doSearch results = do
-  putStrLn "Enter your search hash: "
-  input <- getLine
-  let result = search input results
-  putStrLn "Found similar hashes: "
-  mapM_ print result
-  doSearch results
+doSearch :: Config -> Results -> IO ()
+doSearch config@Config { } results = do
+  putStrLn "Enter your search file:"
+  cur <- getCurrentDirectory
+  file <- (cur </>) <$> getLine
+  putStrLn file
+  mhash <- getHash config file
+  case mhash of
+    Just (hash, _) -> do
+      putStrLn $ "Hash is " ++ show hash
+      case length result of
+        0 -> putStrLn "No similar images found"
+        _ -> do
+          putStrLn "Found similar images:"
+          mapM_ print result
+        where result = search hash results
+    Nothing -> return ()
+  doSearch config results
 
-search :: String -> Results -> [(Int, FilePath)]
+search :: Hash -> Results -> [(Int, FilePath)]
 search hash results = top
   where
-    newHash = Hash (V.fromList hash)
-    y = map (\(h, f) -> (hammingDistance newHash h, f)) results
+    y = map (\(h, f) -> (hammingDistance hash h, f)) results
     top = filter (\(d, f) -> d <= 10) . sortBy (comparing fst) $ y
-  
+
