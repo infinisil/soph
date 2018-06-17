c95c4556ab957ede5fa8362d4cf9d6eab30d387a
Silvan Mosberger <infinisil@icloud.com>
2018-06-18 01:13:01 +0200

    Implement hash dir and feh asks
    Now takes 2 arguments, a hash directory and an import directory
    
    It uses the file names in the hash directory as known hashes, and
    imports files in the import directory one by one, comparing it to the
    known hashes and opening a feh window to delete a potential duplicate
    (press enter to delete). Imports it if it wasn't deleted.

--- src/Main.hs
+++ src/Main.hs
@@ -5,7 +5,6 @@
 
 module Main where
 
-import           Control.Concurrent.Async.Pool
 import           Control.Monad
 import           Control.Monad.Reader
 import           Data.Bits
@@ -13,82 +12,103 @@ import           Data.Char
 import           Data.List
 import           Data.Maybe
 import           Data.Ord
-import           Data.Text                     (Text)
-import qualified Data.Text                     as Text
-import qualified Data.Vector                   as V
+import           Data.Text            (Text)
+import qualified Data.Text            as Text
+import qualified Data.Vector          as V
 import           System.Directory
 import           System.Environment
 import           System.Exit
 import           System.FilePath
-import           System.Process.Text
-
-mapConcurrentlyBounded :: Traversable t => Int -> (a1 -> IO a) -> t a1 -> IO (t a)
-mapConcurrentlyBounded n f args = withTaskGroup n $ flip mapTasks (fmap f args)
+import           System.Process
 
 data Config = Config
   { files     :: [FilePath]
   , blockhash :: FilePath
+  , hashdir   :: FilePath
+  , feh       :: FilePath
   } deriving Show
 
 getConfig :: IO Config
 getConfig = do
-  dir <- fromMaybe (fail "No argument supplied to use as a directory") . listToMaybe <$> getArgs
+  args <- getArgs
+  (hashdir, importdir) <- case args of
+    [] -> fail "No first argument supplied, should be hash directory"
+    [hashdir] -> fail "No second argument supplied, should be import directory"
+    hashdir:importdir:_ -> return (hashdir, importdir)
+  files <- fmap (importdir </>) <$> listDirectory importdir
   blockhash <- fromMaybe (fail "No hashblock binary found in PATH") <$> findExecutable "blockhash"
-  files <- fmap (dir </>) <$> listDirectory dir
+  feh <- fromMaybe (fail "No feh binary found in PATH") <$> findExecutable "feh"
   return Config
     { files = files
     , blockhash = blockhash
+    , feh = feh
+    , hashdir = hashdir
     }
 
-getHash :: Config -> FilePath -> IO (Maybe (Hash, FilePath))
+getHash :: Config -> FilePath -> IO (Maybe Hash)
 getHash Config { blockhash } file = do
-  (code, stdout, stderr) <- readProcessWithExitCode blockhash [ file ] Text.empty
+  (code, stdout, stderr) <- readProcessWithExitCode blockhash [ file ] ""
   case code of
     ExitFailure code -> do
       putStrLn $ "Error code " ++ show code ++ " while getting hash of file " ++ file ++ ": " ++ show stderr ++ show stdout
       return Nothing
-    ExitSuccess   -> return $ Just (Hash (V.fromList (Text.unpack (fst (Text.breakOn "  " stdout)))), file)
+    ExitSuccess -> return $ Just . V.fromList . takeWhile (/= ' ') $ stdout
 
-getHashes :: Config -> IO [(Hash, String)]
-getHashes config@Config { files } = catMaybes <$> mapConcurrentlyBounded 8 (getHash config) files
-
-type Results = [(Hash, FilePath)]
-
-newtype Hash = Hash { unHash :: V.Vector Char } deriving Show
+type Hash = V.Vector Char
 
 hammingDistance :: Hash -> Hash -> Int
-hammingDistance (Hash h1) (Hash h2) =
+hammingDistance h1 h2 =
   V.sum $ V.map popCount $ V.zipWith xor (V.map digitToInt h1) (V.map digitToInt h2)
+
+type State = ([Hash], [FilePath])
+
+initial :: Config -> IO State
+initial Config { hashdir, files } = do
+  hashfiles <- listDirectory hashdir
+  return (V.fromList . dropExtensions <$> hashfiles, files)
+
+trans :: Config -> State -> IO (Maybe State)
+trans config (hashes, []) = do
+  putStrLn "All hashes imported"
+  return Nothing
+trans config@Config { hashdir, feh } (hashes, file:xs) = do
+  mnewhash <- getHash config file
+  case mnewhash of
+    Nothing -> do
+      putStrLn $ "Skipping " ++ file
+      return $ Just (hashes, xs)
+    Just newhash -> do
+      let results = search newhash hashes
+      unless (null results) $ do
+        putStrLn $ "Delete the ones you don't want of " ++ show results ++ " or the new file " ++ file ++ " (with hash " ++ V.toList newhash ++ ")"
+        hashfiles <- mapM (findFileWithHash config) results
+        callProcess feh (hashfiles ++ [ file ] ++ ["-A", "rm %F"])
+      filethere <- doesFileExist file
+      when filethere $ do
+        putStrLn $ "Moving file " ++ file ++ " to hashdir with hash " ++ show newhash
+        renameFile file (hashdir </> V.toList newhash ++ takeExtensions file)
+      return $ Just (newhash : hashes, xs)
+
+findFileWithHash :: Config -> Hash -> IO FilePath
+findFileWithHash config@Config{ hashdir } h = do
+  all <- listDirectory hashdir
+  maybe (fail $ "Couldn't find hash " ++ chars) return . listToMaybe . map (hashdir </>) . filter ((== chars) .  dropExtensions) $ all
+  where chars = V.toList h
 
 main :: IO ()
 main = do
   config <- getConfig
-  putStrLn "Calculating Hashes.."
-  result <- getHashes config
-  doSearch config result
+  putStrLn "initializing"
+  state <- initial config
+  loop config state
 
-doSearch :: Config -> Results -> IO ()
-doSearch config@Config { } results = do
-  putStrLn "Enter your search file:"
-  cur <- getCurrentDirectory
-  file <- (cur </>) <$> getLine
-  putStrLn file
-  mhash <- getHash config file
-  case mhash of
-    Just (hash, _) -> do
-      putStrLn $ "Hash is " ++ show hash
-      case length result of
-        0 -> putStrLn "No similar images found"
-        _ -> do
-          putStrLn "Found similar images:"
-          mapM_ print result
-        where result = search hash results
-    Nothing -> return ()
-  doSearch config results
+loop :: Config -> State -> IO ()
+loop config state = do
+  mnextstate <- trans config state
+  case mnextstate of
+    Nothing        -> return ()
+    Just nextstate -> loop config nextstate
 
-search :: Hash -> Results -> [(Int, FilePath)]
-search hash results = top
-  where
-    y = map (\(h, f) -> (hammingDistance hash h, f)) results
-    top = filter (\(d, f) -> d <= 10) . sortBy (comparing fst) $ y
+search :: Hash -> [Hash] -> [Hash]
+search hash = map snd . filter ((10 >=) . fst) . sortBy (comparing fst) . map (\h -> (hammingDistance hash h, h))
 
