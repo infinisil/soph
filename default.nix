0bfa01efa17ee700ab4075c5736b29382a60d333
Silvan Mosberger <infinisil@icloud.com>
2018-06-18 04:05:37 +0200

    buildable by Nix

---
+++ default.nix
@@ -0,0 +1,28 @@
+{ nixpkgs ? builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs-channels/archive/4b649a99d8461c980e7028a693387dc48033c1f7.tar.gz";
+    sha256 = "0iy2gllj457052wkp20baigb2bnal9nhyai0z9hvjr3x25ngck4y";
+  }
+, pkgs ? import nixpkgs {}
+, lib ? pkgs.lib
+}:
+
+with lib;
+
+let
+
+  hpkgs = import ./stack2nix.nix { inherit pkgs; };
+
+  src = builtins.filterSource (absName: type: let
+    name = removePrefix "${toString ./.}/" absName;
+  in ! (hasSuffix ".stack-work" name
+    || hasSuffix ".git" name
+    || name == "result"
+    || hasSuffix ".nix" name)
+  ) ./.;
+
+  hashsearch = hpkgs.hashsearch.overrideAttrs (oldAttrs: {
+    inherit src;
+  });
+
+in
+  hashsearch
