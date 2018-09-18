96503ce126469b85d767fd54e8bb3c873965c5dc
Silvan Mosberger <infinisil@icloud.com>
2018-09-18 03:00:39 +0200

    Switch to cabal

--- default.nix
+++ default.nix
@@ -1,38 +1,32 @@
-{ nixpkgs ? builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs-channels/archive/4b649a99d8461c980e7028a693387dc48033c1f7.tar.gz";
-    sha256 = "0iy2gllj457052wkp20baigb2bnal9nhyai0z9hvjr3x25ngck4y";
-  }
-, pkgs ? import nixpkgs {}
-, lib ? pkgs.lib
-}:
-
-with lib;
-
 let
-
-  hpkgs = import ./stack2nix.nix { inherit pkgs; };
-
-  src = builtins.filterSource (absName: type: let
-    name = removePrefix "${toString ./.}/" absName;
-  in ! (hasSuffix ".stack-work" name
-    || hasSuffix ".git" name
-    || name == "result"
-    || hasSuffix ".nix" name)
-  ) ./.;
-
-  hashsearch = hpkgs.hashsearch.overrideAttrs (oldAttrs: {
-    inherit src;
-  });
-
-  wrapped = pkgs.symlinkJoin {
-    name = "hashsearch";
-    paths = [ hashsearch ];
-    buildInputs = [ pkgs.makeWrapper ];
-    postBuild = ''
-      wrapProgram $out/bin/hashsearch \
-        --prefix PATH : "${makeBinPath [ pkgs.blockhash pkgs.feh ]}"
-    '';
+  nixpkgsSrc = fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/4b649a99d8461c980e7028a693387dc48033c1f7.tar.gz";
+    sha256 = "0iy2gllj457052wkp20baigb2bnal9nhyai0z9hvjr3x25ngck4y";
   };
 
+  nixpkgs = (import nixpkgsSrc {}).srcOnly {
+    name = "nixpkgs-patched";
+    src = nixpkgsSrc;
+    patches = [
+      # https://github.com/NixOS/nixpkgs/pull/46453
+      (builtins.fetchurl {
+        url = "https://github.com/NixOS/nixpkgs/commit/e6dd03d554e65badd9cdc8b9c137a5998a642e42.patch";
+        sha256 = "0aisra3arv6x6z59dfw4bfxyj40pm6liixgkwpj1rjrr0ql4yc9s";
+      })
+    ];
+  };
 in
-  wrapped
+{ pkgs ? import nixpkgs {}
+}:
+
+(pkgs.haskellPackages.extend (pkgs.haskell.lib.packageSourceOverrides {
+  hashsearch = ./.;
+})).extend (self: super: {
+  hashsearch = super.hashsearch.overrideAttrs (drv: {
+    nativeBuildInputs = drv.nativeBuildInputs or [] ++ [ pkgs.makeWrapper ];
+    postInstall = drv.postInstall or "" + ''
+      wrapProgram $out/bin/hashsearch \
+        --prefix PATH : "${pkgs.lib.makeBinPath [ pkgs.blockhash pkgs.feh ]}"
+    '';
+  });
+}) // { inherit pkgs; }
