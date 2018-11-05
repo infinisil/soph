5471551414f34c21b7f4e08f2056225f6dd63608
Silvan Mosberger <infinisil@icloud.com>
2018-11-05 03:39:14 +0100

    Update nixpkgs and add stack support

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 let
   nixpkgs = fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/0a7e258012b60cbe530a756f09a4f2516786d370.tar.gz";
-    sha256 = "1qcnxkqkw7bffyc17mqifcwjfqwbvn0vs0xgxnjvh9w0ssl2s036";
+    url = "https://github.com/NixOS/nixpkgs/archive/800184df21cfe4924e1e901f387cda49a9a8269c.tar.gz";
+    sha256 = "0xwlbs3sffsjgbc00gadmyi14q1b7ww0fgj0nxa77dip27jzywn1";
   };
 in
 { pkgs ? import nixpkgs {}
--- shell.nix
+++ shell.nix
@@ -3,4 +3,7 @@ with import ./. {};
 hpkgs.shellFor {
   packages = p: [ hpkgs.hashsearch ];
   nativeBuildInputs = [ hpkgs.cabal-install ];
+  shellHook = ''
+    export NIX_PATH=nixpkgs=${toString pkgs.path}
+  '';
 }
